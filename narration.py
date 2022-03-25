d3207bc5316ed110202d87da184c62fc7c9dfc3e
swathipriya123 <99422042+swathipriya123@users.noreply.github.com>
2022-03-25 10:45:53 -0500

    Add files via upload

---
+++ narration.py
@@ -0,0 +1,15 @@
+action('SetNarration(I am going to find the RedKey )')
+action('ShowNarration()')
+action('Wait(3)')
+action('HideNarration()')
+
+action('ShowDialog()')
+action('SetLeft(Kate)')
+action('SetRight(Tom)')
+
+
+action('SetDialog(Kate: I am looking for RedKey )'
+action('SetDialog(Tom : You should not take the RedKey)')
+action('SetDialog(Kate: My King asked me to take the RedKey )')
+action('SetDialog(Tom: You should not Enter this places )')
+action('Wait(5)')
