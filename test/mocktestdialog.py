4f00b3d537d9cb585eafb4eaefea47b9f8d58fda
ckarthala <99357200+ckarthala@users.noreply.github.com>
2022-05-06 01:18:01 -0500

    Add files via upload

---
+++ test/mocktestdialog.py
@@ -0,0 +1,8 @@
+def set_dialog(dialog):
+    return "succeeded SetDialog(Kate: I need healing potion, do you have any?)"
+
+def set_right(character_name):
+    return "succeeded SetRight(Kate)"
+
+def set_left(character_name):
+    return "succeeded SetLeft(Bob)"
