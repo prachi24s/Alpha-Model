f921b1cf237a8c1df57b54aa64bb2d33a72d1347
Chandana <karthala.uss019@gmail.com>
2022-05-05 23:39:03 -0500

    added start_game and dialog

---
+++ camelot_utiles.py
@@ -0,0 +1,151 @@
+from action_execution import *
+from create_character import *
+from create_place import *
+from create_item import *
+from enable_icon import *
+
+
+def setCameraFocus(name):
+    action('SetCameraFocus('+ name + ')')
+    
+
+def start_game():
+    create_place('Storage', 'Storage')
+    create_character('Kate', 'C')
+    character_clothing('Kate', 'Spiky', 'LightArmour')
+    set_character_position('Kate', 'Storage')
+    create_item('Sword', 'Sword')
+    set_position('Sword', 'Storage.Shelf.Left')
+    create_item('BlueKey', 'BlueKey')
+    set_position('BlueKey', 'Storage.Shelf.Right')
+    create_item('Coin', 'Coin')
+    set_position('Coin', 'Storage.Barrel')
+    enableIcon('Select_Sword', 'Select', 'Sword', "Sword", True)
+    enableIcon('Select_BlueKey', 'Select', 'BlueKey', "BlueKey", True)
+    enableIcon('Select_Coin', 'Select', 'Coin', "Coin", True)
+    enableIcon('Exit_Storage', 'Open', 'Storage.Door', 'Enter the Camp', True)
+    
+    create_place('Camp', 'Camp')
+    create_character('Bob', 'F')
+    character_clothing('Bob','Spiky', 'Bandit')
+    create_item('bob_sword', 'Sword')
+    set_position('bob_sword', 'Bob')
+    set_character_position('Bob', 'Camp.RightLog')
+    enableIcon('Talk_Bob', 'Talk', 'Bob', "Talk to the Bob", True)
+    
+    create_place('City', 'City')
+    create_character('Sunny', 'B')
+    character_clothing('Sunny', 'Long', 'Peasant')
+    set_character_position('Sunny', 'City.Bench')
+    sit_character('Sunny', 'City.Bench')
+    enableIcon('Talk_Sunny', 'Talk', 'Sunny', "Talk to the Sunny", True)
+    enableIcon('Exit_City', 'Open', 'City.BrownHouseDoor', 'Enter the Blacksmith', True)
+    
+    create_place('Blacksmith', 'Blacksmith')
+    create_character('Steve', 'D')
+    character_clothing('Steve', 'Spiky', 'Merchant')
+    set_character_position('Steve', 'Blacksmith.Chest')
+    enableIcon('Talk_Steve', 'Talk', 'Steve', "Talk to the Steve", True)
+    create_item('GreenPotion', 'GreenPotion')
+    set_position('GreenPotion', 'Blacksmith.Anvil')
+    create_item('Apple', 'Apple')
+    set_position('Apple','Blacksmith.Table.Left)
+    enableIcon('Select_Apple', 'Select', 'Apple', "Apple", True)
+    enableIcon('Select_GreenPotion', 'Select', 'GreenPotion', "GreenPotion", True)
+    enableIcon('Exit_Blacksmith', 'Open', 'Blacksmith.BackDoor', 'Enter the ForestPath', True)
+    
+    create_place('ForestPath', 'ForestPath')
+    create_character('Tom', 'D')
+    character_clothing('Tom', 'Short', 'Warlock')
+    set_character_position('Tom', 'ForestPath.DirtPile')
+    enableIcon('Talk_Tom', 'Talk', 'Tom', "Talk to the Tom", True)
+    enableIcon('Exit_ForestPath', 'Open', 'ForestPath.WestEnd', 'Enter the Tavern', True)
+    
+    create_place('Bridge', 'Bridge')
+    #enableIcon('Exit_Bridge', 'Open', 'Bridge.NorthEnd' 'Enter the AlchemyShop', True)
+    
+    
+    create_place('AlchemyShop', 'AlchemyShop')
+    create_character('Lily', 'F')
+    character_clothing('Lily', 'Long', 'Witch')
+    set_character_position('Lily', 'AlchemyShop.LeftBookcase')
+    set_character_position('Kate', 'Storage')
+    create_item('lily_sword', 'Sword')
+    set_position('lily_sword', 'Lily')
+    enableIcon('Talk_Lily', 'Talk', 'Lily', "Talk to the Lily", True)
+    create_item('BluePotion', 'BluePotion')
+    set_position('BluePotion', 'AlchemyShop.Table')
+    enableIcon('Select_BluePotion', 'Select', 'BluePotion', "BluePotion", True)
+    enableIcon('Exit_AlchemyShop', 'Open', 'AlchemyShop.Door', 'Enter to CastleBedroom', True)
+    
+    
+    create_place('Tavern', 'Tavern')
+    create_character('Kim', 'D')
+    character_clothing('Kim', 'Short', 'Peasant')
+    #set_character_position('Kate', 'Tavern')
+    set_character_position('Kim', 'Tavern.Chair')
+    enableIcon('Talk_Kim', 'Talk', 'Kim', "Talk to the Kim", True)
+    create_item('Compass', 'Compass')
+    set_position('Compass', 'Tavern.RoundTable')
+    create_item('Scroll', 'Scroll')
+    set_position('Scroll', 'Tavern.Shelf')
+    enableIcon('Select_Scroll', 'Select', 'Scroll', "Scroll", True)
+    enableIcon('Exit_Tavern', 'Open', 'Tavern.BackDoor', 'Enter the Courtyard', True)
+    
+    create_place('Courtyard', 'Courtyard')
+    create_character('Jerry', 'D')
+    character_clothing('Jerry', 'Mage', 'Beggar')
+    set_character_position('Jerry', 'Courtyard.Barrel')
+    create_item('RedPotion', 'RedPotion')
+    set_position('RedPotion', 'Courtyard.SmallStall')
+    create_item('Bottle', 'Bottle')
+    set_position('Bottle', 'Courtyard.SmallStall')
+    enableIcon('Talk_Jerry', 'Talk', 'Jerry', "Talk to the Jerry", True)
+    enableIcon('Exit_Courtyard', 'Open', 'Courtyard.Gate', 'Enter the CastleCrossroads', True)
+    
+    create_place('CastleCrossroads', 'CastleCrossroads')
+    create_character('Mike', 'D')
+    character_clothing('Mike', 'Long', 'HeavyArmour')
+    enableIcon('Talk_Mike', 'Talk', 'Mike', "Talk to the mike", True)
+    enableIcon('Exit_CastleCrossroads', 'Open', 'CastleCrossroads.EastEnd', 'Enter the CastleBedroom', True)
+    
+    create_place('CastleBedroom', 'CastleBedroom')
+    create_character('James', 'B')
+    character_clothing('James', 'Mage_Beard', 'King')
+    set_character_position('James', 'CastleBedroom.Bed')
+    enableIcon('Talk_James', 'Talk', 'James', "Talk to the James", True)
+    
+    create_character('Robert', 'D')
+    character_clothing('Robert', 'Mage_Beard', 'Noble')
+    set_character_position('Robert', 'CastleBedroom.Closet')
+    enableIcon('Talk_Robert', 'Talk', 'Robert', "Talk to the Robert", True)
+    create_item('GoldCup', 'GoldCup')
+    set_position('GoldCup', 'CastleBedroom.Chest')
+    enableIcon('Select_GoldCup', 'Select', 'GoldCup', "GoldCup", True)
+    
+    
+    setCameraFocus('Kate')
+    action('ShowMenu()')
+  
+  
+  
+  
+def menu_list(input_string):
+    if 'Start' in input_string:
+        action('HideMenu()')
+        narration_msg = "Welcome to THE LOST KEY! Your name is Kate and you found the lost key of King's treasure house. The bandit, Bob wants to get key from you. Your goal is to protect key from bandit and other people and bring that key safely to the King."
+        action(f'SetNarration("{narration_msg}")')
+        action('ShowNarration()')
+        action('EnableInput()')
+    elif 'Selected Credits' in input_string:
+        action('ShowCredits()')
+    elif 'Close Credits' in input_string:
+        action('HideCredits()')
+    elif 'Quit' in input_string:
+        action('Quit()')
+    elif 'Close Narration' in input_string:
+        action('HideNarration()')
+    
+    
+
+    
---
+++ create_character.py
@@ -0,0 +1,289 @@
+from action_execution import *
+from create_dialog import *
+from create_place import *
+from camelot_utiles import *
+from create_item import *
+
+def create_character(character_name, body_Type):
+    action('CreateCharacter(' + character_name + ', ' + body_Type + ')' )
+
+def character_clothing(character_name, Hairstyle, Outfit):
+    action('SetHairStyle('+ character_name +', ' + Hairstyle +')')
+    action('SetClothing('+ character_name +', ' + Outfit +')')
+
+def set_character_position(character_name, position_name):
+     action('SetPosition(' + character_name + ', ' + position_name + ')')
+
+def sit_character(character_name, place_name):
+    action('Sit('+ character_name +', '+ place_name +')')
+
+def die_character(character_name):
+    action('Die('+ character_name +')')
+
+def character_drink(character_name):
+    action('Drink('+ character_name +')')
+
+def take_item(character_name, item, Position):
+     action('Take('+ character_name +', '+ item +', '+ Position +')')
+
+def pocket(character_name, item):
+    action('Pocket('+ character_name +', '+ item +')')
+
+def character_attack(character1, character2):
+     action('Attack('+ character1 +', '+ character2 +', false)')
+
+def character_attack_success(character1, character2):   
+     action('Attack('+ character1 +', '+ character2 +', true)')
+
+def give_item(item, character1, character2):
+     action('Give('+ character1 +', '+ item +', '+ character2 +')')
+
+def character_walk(character1, character2):
+    action('WalkTo(' + character1 + ', ' + character2 + ')')
+    
+def draw(character_name, item):
+    action('Draw(' + character_name + ',' + item + ')')
+
+def talktobob(character1, character2):
+    character_walk(character1, character2)
+    set_right(character1)
+    set_left(character2)
+    set_dialog('Bob: Who are you?')
+    set_dialog('Kate: I am Kate')
+    set_dialog('Bob: What are you doing here?')
+    set_dialog('Kate: I am here to take that key and handover it to the king.')
+    set_dialog('Bob:  I want the key...just handover it to me.')
+    set_dialog('Kate: No...just go away from my way\\n[Attack | Attack]')
+    show_dialog()
+    while 1:
+        if 'Attack' in input():
+           select ='Attack'
+           break
+    if select == 'Attack':
+        hide_dialog()
+        draw('Kate', 'Sword')
+        character_attack('Bob','Kate')
+        character_attack('Kate', 'Bob')
+        character_attack('Bob', 'Kate')
+        character_attack_success('Kate', 'Bob')
+        die_character('Bob')
+        pocket('Kate', 'Sword')
+        exit_place('Kate', 'Camp.Exit')
+        enter_place('Kate', 'City.NorthEnd')
+
+def talktosunny(character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('Kate: Hi! How are you?')
+     set_dialog('Sunny: Hey! I am good. You are looking injured what happened with you?')
+     set_dialog('Kate: I had a fight recently thats why I got injured. Can you tell me is there any place where I can get the healing potion?')
+     set_dialog('Sunny: Yeah sure! Its just in front of you. Are you able to see the brown house door? .')
+     set_dialog('Kate: Yes, I can see that.')
+     set_dialog('Sunny: Just enter into that door,you can get the healing potion there.')
+     set_dialog('Kate: Thank you so much!')
+     set_dialog('Sunny: No problem. Take care! \\n[continue | continue]')
+     show_dialog()
+     while 1:
+         if 'continue' in input():
+            select ='continue'
+            break
+     if select == 'continue':
+         hide_dialog()
+         
+
+
+def talktosteve(character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('Steve: Hi! How can I help you?')
+     set_dialog('Kate: I need healing potion, do you have any?')
+     set_dialog('Steve: Yes, I am having one.')
+     set_dialog('Kate: Fine, how much does it cost?')
+     set_dialog('Steve: Its of one gold coin.')
+     set_dialog('Kate: I want to take that potion.')
+     set_dialog('Steve: Okay!\\n[ continue | continue]')
+     show_dialog()
+     while 1:
+         if 'continue' in input():
+            select ='continue'
+            break
+     if select == 'continue':
+         hide_dialog()
+         take_item('Steve', 'GreenPotion', 'Blacksmith.Anvil' )
+         give_item('GreenPotion', 'Steve', 'Kate')
+         give_item('Coin', 'Kate', 'Steve')
+         character_drink('Kate')
+    '''
+    elif 'GreenPotion' in input():
+            hide_dialog()
+            take_item('Steve', 'GreenPotion', 'Blacksmith.Anvil')
+            give_item('GreenPotion', 'Steve', 'Kate')
+            set_right(character1)
+            set_left(character2)
+            show_dialog()
+         
+      '''
+         
+def talktotom(character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('Tom: Give me the key')
+     set_dialog('Kate: I will not give the key... go away from me or else...I will kill you..')
+     set_dialog('\\n[ Attack | Attack]')
+     show_dialog()
+     while 1:
+         if 'Attack' in input():
+            select ='Attack'
+            break
+     if select == 'Attack':
+        hide_dialog()
+        draw('Kate', 'Sword')
+        character_attack('Kate', 'Tom')
+        character_attack('Tom', 'Kate')
+        character_attack_success('Kate', 'Tom')
+        die_character('Tom')
+        pocket('Kate', 'Sword')
+        exit_place('Kate', 'ForestPath.WestEnd')
+        enter_place('Kate', 'Bridge.SouthEnd')
+
+def talktolily(character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('Lily: Kate, You found the key...I want it..just give it to me otherwise it will not be good for you.')
+     set_dialog('Kate: No, I cant give it to you.')
+     set_dialog('Lily: Okay! Lets see...\\n[ Attack | Attack]')
+     show_dialog()
+     while 1:
+         if 'continue' in input():
+            select ='continue'
+            break
+     if select == 'Attack':
+        hide_dialog()
+        draw('Kate', 'Sword')
+        character_attack('Lily', 'Kate')
+        character_attack('Kate', 'Lily')
+        character_attack('Kate', 'Lily')
+        character_attack_success('Kate', 'Lily')
+        die_character('Lily')
+        pocket('Kate', 'Sword')
+        exit_place('Kate', 'AlchemyShop.BackDoor')
+        enter_place('Kate', 'CastleBedroom.Door')
+
+
+def talktokim( character1, character2):
+    character_walk(character1, character2)
+    set_right(character1)
+    set_left(character2)
+    set_dialog('Kim: Who are you?')
+    set_dialog('Kate: I am Kate')
+    set_dialog('Kim: why did you come hear ?')
+    set_dialog('Kate: I need your help....')
+    set_dialog('Kim:  What help do you need ?')
+    set_dialog('Kate: I need direction of a place for theat i need Campass can you please give me campass')
+    set_dialog('Kim:  ok..I will help you..heare is the compass\\n[continue | continue]')
+    show_dialog()
+    while 1:
+        if 'continue' in input():
+           select ='continue'
+           break
+    if select == 'continue':
+        hide_dialog()
+        take_item('Kim', 'Compass', 'Tavern.RoundTable' )
+        give_item('Compass', 'Kim', 'Kate')
+        pocket('Kate', 'Compass')
+
+def talktojerry(character1, character2):
+    character_walk(character1, character2)
+    set_right(character1)
+    set_left(character2)
+    set_dialog('Jerry: Hi! I am serving food and drink for free would you like to have something?\\n[RedPotion | RedPotion] \\n[Bottle | Bottle]')
+    show_dialog()
+    while 1:
+        if 'RedPotion' in input():
+            hide_dialog()
+            take_item('Jerry', 'RedPotion', 'Courtyard.SmallStall')
+            give_item('RedPotion', 'Jerry', 'Kate')
+            character_drink('Kate')
+            die_character('Kate')
+            set_position('BlueKey', 'Courtyard.Barrel')
+            take_item('Jerry', 'BlueKey', 'Courtyard.Barrel')
+            break
+        elif 'Bottle' in input():
+            hide_dialog()
+            take_item('Jerry', 'Bottle', 'Courtyard.SmallStall')
+            give_item('Bottle', 'Jerry', 'Kate')
+            set_right(character1)
+            set_left(character2)
+            set_dialog('Jerry: Give me that key')
+            set_dialog('Jerry: Hi! I am serving food and drink for free would you like to have something?\\n[Attack | Attack]')
+            show_dialog()
+            while 1:
+                if 'Attack' in input():
+                    hide_dialog()
+                    character_attack('Kate', 'Jerry')
+                    character_attack('Jerry', 'Kate')
+                    character_attack_success('Kate', 'Jerry')
+                    exit_place('Kate', 'Courtyard.Gate' )
+                    enter_place('Kate', 'CastleCrossroads.WestEnd')
+                    break
+        
+
+def talktoMike( character1, character2):
+    character_walk(character1, character2)
+    set_right(character1)
+    set_left(character2)
+    set_dialog('Mike: Hello Kate your back to the kingdom')
+    set_dialog('Kate: Yes...james i got the lost key')
+    set_dialog('Mike: go ... meet the king he will appreciate you')
+    set_dialog('Kate:  yes i will handover the key to king \\n[ Go | Go]')
+    show_dialog()
+    while 1:
+        if 'continue' in input():
+           select ='continue'
+           break
+    if select == 'continue':
+        hide_dialog()
+        exit_place('Kate', 'CastleCrossroads.Gate')
+        enter_place('Kate', 'CastleBedroom.Door')
+        
+         
+
+def talktojames( character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('Kate: Hello king James...I found the lost key')
+     set_dialog('James: Ohh...Thats great! Where is it?')
+     set_dialog('Kate: Here it is')
+     set_dialog('James: Thank you so much Kate.I want to give you reward for your work.\\n[continue | continue]')
+     set_dialog('Thank you king James!')
+     show_dialog()
+     while 1:
+         if 'continue' in input():
+            select ='continue'
+            break
+     if select == 'continue':
+         hide_dialog()
+         give_item('BlueKey', 'Kate', 'James')
+         
+         
+def talktorobert( character1, character2):
+     character_walk(character1, character2)
+     set_right(character1)
+     set_left(character2)
+     set_dialog('James: Robert, give me things which are in the chest?')
+     set_dialog('Robert: Sure King!\\n[continue | continue]')
+     show_dialog()
+     while 1:
+         if 'continue' in input():
+            select ='continue'
+            break
+     if select == 'continue':
+         hide_dialog()
+         give_item('GoldCup', 'James', 'Kate')
+         
+
